418b89ccb557d2d058cabdcb6cd286aa79822f54
wlnhoge <georg.hoeller@raiffeisenbank.at>
2023-07-27 21:52:32 +0200

    add req.headers response

--- index.js
+++ index.js
@@ -24,7 +24,29 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+  // console.log('api/whoami/ with object \n',req)
+  // console.log('api/whoami/ with object \n',req.headers)
+  // console.log('api/whoami/ with object \n',req.headers['user-agent'])
+  // console.log('api/whoami/ with object \n',req.headers['accept-language'])
+  // console.log('api/whoami/ with object \n', Object.keys(req) )
+  
+  res.json({
+    ipaddress: req.ip,
+    language:  req.headers['user-agent'],
+    software:  req.headers['accept-language']
+  });
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+// WHOAMI: 
+// json structure: {ipaddress :, language ,: software : }
+// {"ipaddress":"159.20.14.100",
+// "language":"en-US,en;q=0.5", 
+// "software": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
